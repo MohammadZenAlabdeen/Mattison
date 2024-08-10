dc0a549b65742b95eeb1df4d1035b6885e6979d2
MohammadZenAlabdeen <myn1ytc@gmail.com>
2024-08-10 17:19:19 +0300

    floating

--- src/Components/MainContainer/Card/Card.jsx
+++ src/Components/MainContainer/Card/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Card.css'
+import { FaRegHandPointer } from "react-icons/fa6";
 import { useState } from 'react'
 const Card = ({image,title}) => {
 const [flip,setFlip]= useState('');
@@ -7,14 +8,22 @@ const [flip,setFlip]= useState('');
     <div className='card-container'>
         <div className='card-main-contain'>
             <div className={'the-card '+flip} onClick={()=>{flip==='' ? setFlip('flip'):setFlip('')}}>
-            <div className='front'></div>
-            <div className='back'>hello</div>
+            <div className='front'>
+                <div className='pointer'>
+                <FaRegHandPointer/>
+                </div>
+            </div>
+            <div className='back'>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum necessitatibus asperiores minus magni sint, illum esse quos hic aperiam repellat autem dolorem ab qui a, distinctio laboriosam ullam eius in.</p>
+            </div>
             </div>
 
         </div>
+        <div className='title-contain'>
         <h1 className='title'>
             {title}
         </h1>
+        </div>
     </div>
   )
 }
--- src/Components/MainContainer/MainContainer.jsx
+++ src/Components/MainContainer/MainContainer.jsx
@@ -4,10 +4,13 @@ import Card from './Card/Card'
 const MainContainer = () => {
   return (
     <div>
-    <Card title="hello"></Card>
-    <Card title="hello"></Card>
-    <Card title="hello"></Card>
-    <Card title="hello"></Card>
+    <Card title="Software Services"></Card>
+    <Card title="Designing"></Card>
+    <Card title="Platform"></Card>
+    <Card title="Academy"></Card>
+    <Card title="SMMA"></Card>
+    <Card title="Coming Soon"></Card>
+    <Card title="Project X"></Card>
     </div>
   )
 }
