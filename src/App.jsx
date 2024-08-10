c1177bcd5a5363d4f23865263980f52830a33730
Ghaith-Alqattan <ghaith.alqattan04@gmail.com>
2024-08-10 18:15:22 +0300

    nav update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { useState } from 'react'
 import Landing from './Pages/Landing/Landing'
-
-
 function App() {
-
   return (
     <>
       <Landing></Landing>
-      <p>check</p>
     </>
   )
 }
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import "./Nav.css"
-const Nav = () => {
+const Nav = ({ logo, navItems }) => {
   return (
     <>
-      <p>nav</p>
+      <nav>
+        <div className="logo">
+          <img src={logo} alt={logo} className="logoImage" />
+        </div>
+        <ul className="links">
+          {navItems.map((element, index) => {
+            return (<li key={index}>
+              <a href={element.link}>{element.link}</a>
+            </li>)
+          })}
+        </ul>
+      </nav>
     </>
   )
 }
